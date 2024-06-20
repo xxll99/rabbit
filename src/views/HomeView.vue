4d7fd6e158c4b292c2265f27e30b0044e25237db
xxll99 <2439991987@qq.com>
2024-06-20 16:44:42 +0800

    我的测试

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+const date=new Date()
+let formDate=date.format('yyyyMMdd')
+console.log(formDate)
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <textarea maxlength="1000"></textarea>
   </main>
 </template>
