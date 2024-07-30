5b2410702bd112e4f3cf47634f471d56c64bb191
xxll99 <2439991987@qq.com>
2024-07-30 16:33:54 +0800

    密码输入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,24 @@
 import TheWelcome from '../components/TheWelcome.vue'
 
 const date=new Date()
+let input = ref('')
+
 let formDate=date.format('yyyyMMdd')
 console.log(formDate)
+
+function Password(e){
+  if(e.key==13){
+    console.log(input.value)
+  }
+}
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
     <textarea maxlength="1000"></textarea>
+    <el-input type="password" v-model="input" placeholder="请输入内容" @keydown="Password"/>
+
   </main>
 </template>
